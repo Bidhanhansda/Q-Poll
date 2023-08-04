64ce057d4dbcd4f2286b7c88c4bc7f849512246d
Bidhanhansda <bidhanhansda1997@gmail.com>
2023-08-04 18:01:01 +0530

    deployed q-poll

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {io} from "socket.io-client";
 
-const SOCKET_URL = "http://localhost:5001";
+const SOCKET_URL = "https://q-poll.onrender.com";
 export const socket = io(SOCKET_URL);
 
 export const AppContext = React.createContext();
